068bab4c408758087a243ef9f33b8650991a2444
Chirag-Punia <cpunia_be21@thapar.edu>
2024-04-14 01:40:39 +0530

    homePage setup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import React, { Component } from 'react'
-import "./App.css"
-import { BrowserRouter, Route,Routes } from 'react-router-dom'
-import Home from "./pages/Home"
-import EditorPage from "./pages/EditorPage"
-
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import EditorPage from "./pages/EditorPage";
+import { Toaster } from "react-hot-toast";
 
 export default class App extends Component {
   render() {
     return (
       <>
-      <BrowserRouter>
-      <Routes>
-        <Route path ="/" element={<Home />}/>
-        <Route path ="/editor" element={<EditorPage />}/>
-      </Routes>
-      </BrowserRouter>
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            success: {
+              theme: {
+                primary: "#eee",
+              },
+            },
+          }}
+        ></Toaster>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/editor" element={<EditorPage />} />
+          </Routes>
+        </BrowserRouter>
       </>
-    )
+    );
   }
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,30 +1,55 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
+import "./Home.css";
+import { v4 as uuidV4 } from "uuid";
+import toast from "react-hot-toast";
 
-export default class Home extends Component {
-  render() {
-    return (
-      <div className="homepageWrapper">
-        <div className="formWrapper">
-          <img src="./logo.jpeg" alt="logo" />
-          <h4 className="mainLabel">Paste Invitation Room ID</h4>
-          <div className="inputGroup">
-            <input type="text" className="inputBox" placeholder="ROOM ID" />
-            <input type="text" className="inputBox" placeholder="USERNAME" />
-            <button className="btn joinBtn">JOIN</button>
-            <span>
-              if you don't have an invite then create &nbsp;
-              <a href="" className="createNewBtn">
-                New Room
-              </a>
-            </span>
-          </div>
+const Home = () => {
+  const [roomId, setroomId] = useState("");
+  const [username, setUserName] = useState("");
+  const createNewRoom = (e) => {
+    e.preventDefault();
+    const id = uuidV4();
+    setroomId(id);
+    toast.success("Created a new room");
+  };
+  return (
+    <div className="homepageWrapper">
+      <div className="formWrapper">
+        <img src="./logo.png" alt="logo" />
+        <h4 className="mainLabel">Paste Invitation Room ID</h4>
+        <div className="inputGroup">
+          <input
+            type="text"
+            className="inputBox"
+            placeholder="ROOM ID"
+            onChange={(e) => {
+              setroomId(e.target.value);
+            }}
+            value={roomId}
+          />
+          <input
+            type="text"
+            className="inputBox"
+            placeholder="USERNAME"
+            value={username}
+            onChange={(e) => {
+              setUserName(e.target.value);
+            }}
+          />
+          <button className="btn joinBtn">JOIN</button>
+          <span className="createInfo">
+            if you don't have an invite then create &nbsp;
+            <a onClick={createNewRoom} href="" className="createNewBtn">
+              new room
+            </a>
+          </span>
         </div>
-        <footer>
-          <h4>
-            Made with ❤️
-          </h4>
-        </footer>
       </div>
-    );
-  }
-}
+      <footer>
+        <h4>Made with ❤️</h4>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
