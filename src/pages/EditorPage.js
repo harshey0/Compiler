f335532b723549b31931bcf21536397e4f21d77b
Chirag-Punia <cpunia_be21@thapar.edu>
2024-04-14 10:57:33 +0530

    Client bug

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,9 +1,41 @@
-import React, { Component } from 'react'
+import React, { useState } from "react";
+import "./styles/EditorPage.css";
+import Client from "../components/Client";
 
-export default class EditorPage extends Component {
-  render() {
+const EditorPage = () => {
+  {
+    const [clients, setClients] = useState([
+      {
+        socketId: 1,
+        username: "Chirag",
+      },
+      {
+        socketId: 2,
+        username: "Harshit",
+      },
+    ]);
     return (
-      <div>EditorPage</div>
-    )
+      <>
+        <div className="mainWrap">
+          <div className="aside">
+            <div className="asideInner">
+              <div className="logo">
+                <img className="logoImage" src="/logo.png" alt="editorlogo" />
+              </div>
+              <h3>Connected</h3>
+              <div className="clientList">
+              <Client  props={"Chirag Punia"} />
+                {clients.map((client) => {
+                  <Client key={client.socketId} props={client.username} />;
+                })}
+              </div>
+            </div>
+          </div>
+          <div className="editorWrap">Editor Goes here ....</div>
+        </div>
+      </>
+    );
   }
-}
+};
+
+export default EditorPage;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,34 @@
 import React, { Component, useState } from "react";
-import "./Home.css";
+import "./styles/Home.css";
 import { v4 as uuidV4 } from "uuid";
 import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
   const [roomId, setroomId] = useState("");
   const [username, setUserName] = useState("");
+  const handleEnter = (e) => {
+    if (e.code === "Enter") {
+      joinRoom();
+    }
+  };
   const createNewRoom = (e) => {
     e.preventDefault();
     const id = uuidV4();
     setroomId(id);
     toast.success("Created a new room");
+  };
+  const joinRoom = () => {
+    if (!roomId || !username) {
+      toast.error("Room Id and Username is required");
+      return;
+    }
+    navigate(`/editor/${roomId}`, {
+      state: {
+        username,
+      },
+    });
   };
   return (
     <div className="homepageWrapper">
@@ -26,17 +44,21 @@ const Home = () => {
               setroomId(e.target.value);
             }}
             value={roomId}
+            onKeyUp={handleEnter}
           />
           <input
             type="text"
             className="inputBox"
             placeholder="USERNAME"
+            onKeyUp={handleEnter}
             value={username}
             onChange={(e) => {
               setUserName(e.target.value);
             }}
           />
-          <button className="btn joinBtn">JOIN</button>
+          <button className="btn joinBtn" onClick={joinRoom}>
+            JOIN
+          </button>
           <span className="createInfo">
             if you don't have an invite then create &nbsp;
             <a onClick={createNewRoom} href="" className="createNewBtn">
